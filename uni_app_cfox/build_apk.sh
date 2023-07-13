7b11ccccfd21cc9253b92c0816812a6bef19d7c0
a-bert <bert-an@outlook.com>
2023-07-13 13:30:26 +0800

    更新依赖

--- uni_app_cfox/build_apk.sh
+++ uni_app_cfox/build_apk.sh
@@ -5,9 +5,9 @@ start_time=$(date +%s)
 # 环境：开发、测试、预生产、生产
 env_arr=("dev" "uat" "pre" "pro")
 # 渠道名称:
-channel_name_arr=("官方" "内部" "cfa01" "cfa02" "DAACD" )
+channel_name_arr=("官方" "内部" "cfa01" "cfa02" "DAACD")
 # 渠道码:
-channel_code_arr=("official" "momo6" "T05xcj" "CFA02" "DAACD" )
+channel_code_arr=("official" "momo6" "T05xcj" "CFA02" "DAACD")
 
 #app_typ
 app_type="cfox"
@@ -19,9 +19,8 @@ echo "======================请选择env====================="
 echo "1：dev-开发、2：uat-测试、3：pre-预生产、4：pro-生产"
 echo "===================================================="
 # 使用 select 命令让用户选择参数
-select env in "${env_arr[@]}"
-do
-  if [[ $env ]];then
+select env in "${env_arr[@]}"; do
+  if [[ $env ]]; then
     break
   else
     echo "请重新选择"
@@ -33,39 +32,27 @@ echo "环境env:$env"
 echo "================================请选择渠道码==============================="
 channel_str="0：ALL——所有渠道"
 channel_len=${#channel_code_arr[@]}
-for(( i=0; i<"${channel_len}"; i++)) do
-  channel_str="${channel_str}\n$((i+1))：${channel_code_arr[i]}——${channel_name_arr[i]}渠道"
-  if (($((i)) == $((channel_len-1)) )); then
+for ((i = 0; i < "${channel_len}"; i++)); do
+  channel_str="${channel_str}\n$((i + 1))：${channel_code_arr[i]}——${channel_name_arr[i]}渠道"
+  if (($((i)) == $((channel_len - 1)))); then
     channel_str="${channel_str}"
   fi
 done
 echo -e "$channel_str"
 echo "========================================================================"
 read -r -t 3 ch_num
-if [  -z "${ch_num}" ];then
-	ch_num=0
+if [ -z "${ch_num}" ]; then
+  ch_num=0
 fi
-while(( "${ch_num}" < "0" || $((ch_num)) > $((channel_len)) ))
-do
+while (("${ch_num}" < "0" || $((ch_num)) > $((channel_len)))); do
   echo "输入错误，请输入0到${channel_len} ！！！"
   read -r ch_num
 done
 if [ "${ch_num}" == "0" ]; then
   echo "渠道码：ALL "
 else
-  echo "渠道包 ${channel_code_arr[$((ch_num-1))]}"
+  echo "渠道包 ${channel_code_arr[$((ch_num - 1))]}"
 fi
-
-echo "是否执行clean(默认:n) [ y/n ]"
-read -r -t 3 is_clean
-if [ -z "${is_clean}" ];then
-	is_clean="n"
-fi
-while [[ "${is_clean}" != "y" ]] && [[ ${is_clean} != "n" ]]
-do
-  echo "输入错误，请输入[ y/n ] ！"
-  read -r is_clean
-done
 
 #---------------------构建位置----------------#
 #当前工程绝对路径
@@ -84,24 +71,24 @@ fi
 #创建目录
 mkdir -p "${apk_path}"
 
-if [ "${is_clean}" = "y" ];then
-  echo "====================== 开始清理 ======================="
-	flutter clean
-fi
+echo "====================== 开始清理 ======================="
+flutter clean
+echo "====================== 更新依赖 ======================="
+flutter pub get
 
 # 开始打包
-if (($((ch_num)) == 0 )); then
+if (($((ch_num)) == 0)); then
   echo "================= 开始构建：全部渠道包 ================="
   channel=""
-  for(( i=0;i<$((channel_len));i++)) do
+  for ((i = 0; i < $((channel_len)); i++)); do
     channel=${channel_code_arr[$i]}
     echo "正在构建：${channel} 渠道包"
     flutter build apk --dart-define=PACKAGE_ENV="${env}" --dart-define=CHANNEL="${channel}" --target-platform android-arm --split-per-abi --release --flavor "${env}"
     # 移动apk到指定文件
     cp -R "${release_path}"*.apk "${apk_path}"
-  done;
+  done
 else
-  channel=${channel_code_arr[$((ch_num-1))]}
+  channel=${channel_code_arr[$((ch_num - 1))]}
   echo "============= 正在构建：${channel} 渠道包 =============="
   flutter build apk --dart-define=PACKAGE_ENV="${env}" --dart-define=CHANNEL="${channel}" --target-platform android-arm --split-per-abi --release --flavor "${env}"
   # 移动apk到指定文件
