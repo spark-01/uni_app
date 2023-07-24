9fbe5ef40aad9c152349d23e437378052670c491
a-bert <bert-an@outlook.com>
2023-07-24 21:46:32 +0800

    完善

--- uni_app_dm/test/widget_test.dart
+++ uni_app_dm/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:uni_app_dm/main.dart';
+import 'package:uni_app_base/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
